1d176b87be8b0bde8d0d9530dd135c2faa9ba9d1
Maxim <maxim@aeromultimedia.com>
2015-12-07 22:13:05 +0300

    KafkaStrimmer works

--- src/main/scala/com/test/core/streaming/KafkaStrimmer.scala
+++ src/main/scala/com/test/core/streaming/KafkaStrimmer.scala
@@ -28,7 +28,7 @@ import org.apache.spark.SparkConf
 
 /**
  * Consumes messages from one or more topics in Kafka and does wordcount.
- * Usage: KafkaWordCount <zkQuorum> <group> <topics> <numThreads>
+ * Usage: KafkaStrimmer <zkQuorum> <group> <topics> <numThreads>
  *   <zkQuorum> is a list of one or more zookeeper servers that make quorum
  *   <group> is the name of kafka consumer group
  *   <topics> is a list of one or more kafka topics to consume from
@@ -36,29 +36,29 @@ import org.apache.spark.SparkConf
  *
  * Example:
  *    `$ bin/run-example \
- *      org.apache.spark.examples.streaming.KafkaWordCount zoo01,zoo02,zoo03 \
+ *      org.apache.spark.examples.streaming.KafkaStrimmer zoo01,zoo02,zoo03 \
  *      my-consumer-group topic1,topic2 1`
  */
-object KafkaWordCount {
+object KafkaStrimmer {
   def main(args: Array[String]) {
     if (args.length < 4) {
-      System.err.println("Usage: KafkaWordCount <zkQuorum> <group> <topics> <numThreads>")
+      System.err.println("Usage: KafkaStrimmer <zkQuorum> <group> <topics> <numThreads>")
       System.exit(1)
     }
 
-    // StreamingExamples.setStreamingLogLevels()
-
+    // params
     val Array(zkQuorum, group, topics, numThreads) = args
-    val sparkConf = new SparkConf().setAppName("KafkaWordCount")
+    // Spark app configuration
+    val sparkConf = new SparkConf().setAppName("KafkaStrimmer")
+    // creating entry point for Spark Streaming
     val ssc = new StreamingContext(sparkConf, Seconds(2))
+    // shame on me... magic
     ssc.checkpoint("checkpoint")
 
     val topicMap = topics.split(",").map((_, numThreads.toInt)).toMap
-    val lines = KafkaUtils.createStream(ssc, zkQuorum, group, topicMap).map(_._2)
-    val words = lines.flatMap(_.split(" "))
-    val wordCounts = words.map(x => (x, 1L))
-      .reduceByKeyAndWindow(_ + _, _ - _, Minutes(10), Seconds(2), 2)
-    wordCounts.print()
+    val lines = KafkaUtils.createStream(ssc, zkQuorum, group, topicMap)
+
+    lines.print()
 
     ssc.start()
     ssc.awaitTermination()
